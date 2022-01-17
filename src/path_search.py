e12d06c91d5d26934888af0db7ca8a103de56ad5
Dheeraj <dheerusatya.01@gmail.com>
2022-01-17 23:29:39 +0530

    minor changes-2

--- src/path_search.py
+++ src/path_search.py
@@ -19,6 +19,7 @@ import numpy as np
 
 
 def back_grid(msg):
+    # create an object to store map data
     global oc_grid
     grid_oc= msg.data
 
@@ -29,22 +30,14 @@ def back_grid(msg):
 
     #return grid_oc
 
-def goal_callback(target):
-    global goal
-    dx = target.pose.position.x
-    dy = target.pose.position.y
-    dz = target.pose.position.z
-    goal = (dx,dy)
-    # goal = Point()
-    # goal = target.pose.position
-    # return goal
+
 
 def start_callback(loc):
     global pos
     ox = loc.pose.pose.position.x
     oy = loc.pose.pose.position.y
-    oz = loc.pose.pose.position.z    
-    pos = (ox,oy)
+    #oz = loc.pose.pose.position.z    
+    pos = [ox,oy]
     # pos = Point()
     # # pos.x = loc.pose.pose.x
     # # pos.y = loc.pose.pose.y
@@ -52,11 +45,20 @@ def start_callback(loc):
     # pos = loc.pose.pose.position
     # return pos
 
+def goal_callback(target):
+    global goal
+    dx = target.pose.position.x
+    dy = target.pose.position.y
+    
+    goal = [dx,dy]
+    # goal = Point()
+    # goal = target.pose.position
+    # return goal
+
 def path_search(origin,destination):
     
     while not rospy.is_shutdown():
-        
-    
+
         path= a_star_search(origin,destination,oc_grid)
 
         plot_path.plot(path)
@@ -64,30 +66,34 @@ def path_search(origin,destination):
 
 if __name__ == '__main__':
     try:
+        res= 0.030054
         rospy.init_node('path_finder', anonymous=True)
         # obtaining start position 
         rospy.wait_for_message("/ground_truth/state",Odometry)
         ori_sub = rospy.Subscriber('/ground_truth/state',Odometry, start_callback)
-        
+        rospy.Rate(1)
         #origin = pos
         # obtaining goal position
         rospy.wait_for_message("/move_base_simple/goal",PoseStamped)
-        destin_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, goal_callback )
+        destin_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, goal_callback)
+        rospy.Rate(1)
+        x2 = goal[0] 
+        y2 = goal[1]
+        col2, row2= int((x2+50.01)/res) , int((y2+50.01)/res)
+        destination = (row2,col2)
         #destination= goal    
-        # create an object to store map data
+        
         rospy.wait_for_message("/map",OccupancyGrid)
         grid_sub= rospy.Subscriber('/map', OccupancyGrid, back_grid)
+        rospy.Rate(1)
 
-        res= 0.030054
+        
         # converting real-time coordinates to occupancy grid indices 
         x1,y1 = pos[0], pos[1]
         col1, row1= int((x1+50.01)/res) , int((y1+50.01)/res)
         origin = (row1,col1)
 
-        x2 = goal[0] 
-        y2 = goal[1]
-        col2, row2= int((x2+50.01)/res) , int((y2+50.01)/res)
-        destination = (row2,col2)
+        
 
         path_search(origin,destination)
     
